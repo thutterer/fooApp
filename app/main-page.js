e2d81be351c61ec0bcdfbd841ca6f2af0d987020
Thomas Hutterer <thutterer@suse.de>
2016-06-24 15:19:08 +0200

    create and delete texts

--- app/main-page.js
+++ app/main-page.js
@@ -1,7 +1,34 @@
-var observable = require("data/observable");
+var Observable = require("data/observable").Observable;
+var ObservableArray = require("data/observable-array").ObservableArray;
+var pageContext = new Observable({
+    newText: 'foo bar baz',
+    textList: new ObservableArray([
+        { text: "foo" },
+        { text: "bar" },
+        { text: "baz" }
+    ])
+});
+
 function pageLoaded(args) {
+    
     var page = args.object;
-    var emptyContext = new observable.Observable();
-    page.bindingContext = emptyContext;
+
+    page.bindingContext = pageContext;
+}
+function saveText() {
+    pageContext.textList.push({ text: pageContext.newText });
+    pageContext.newText = "";
+}
+function listViewItemTap(item) {
+    var dialogs = require("ui/dialogs");
+    dialogs.action("", "Cancel", ["Delete"]).then(function (result) {
+        switch(result) {
+            case "Delete":
+                pageContext.textList.splice(item.index, 1);
+                break;
+        } 
+    });
 }
 exports.pageLoaded = pageLoaded;
+exports.saveText = saveText;
+exports.listViewItemTap = listViewItemTap;
